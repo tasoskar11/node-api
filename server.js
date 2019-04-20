718bf656ab0779d34fb1a7c6dbf4d34ce1e00726
anka07 <anka07@reven.mac.local>
2019-04-20 23:57:14 +0200

    Added application middleware for handling the film titles and returning youtube links

--- server.js
+++ server.js
@@ -25,6 +25,24 @@ app.use((req, res, next) => {
 app.use('/health', require('./src/utils/health'))
 app.use(metricsMiddleware)
 
+// application middleware
+app.get('/pc-se/film/:title', (req, res) => {
+  getImdb(req.params.title)
+    .then(imdb => getMovie(imdb._embedded['viaplay:blocks'][0]._embedded['viaplay:product'].content.imdb.id))
+    .then(movie => getTrailer(movie.movie_results[0].id))
+    .then((trailer) => {
+      // console.log('trailer', trailer)
+      res.header('Cache-Control', 'public, max-age=3600')
+      res.set('Content-Type', 'application/json')
+      res.send({ title: `https://www.youtube.com/watch?v=${trailer.results[0].key}` })
+    })
+    .catch((err) => {
+      console.log('Trailer API error: ', err.message)
+      res.header('Cache-Control', 'public, max-age=0')
+      res.status(err.message.status || 500).end()
+    })
+})
+
 
 const port = process.argv[2] || process.env.PORT || 5001
 const server = http.createServer(app);
