20ee4777fb8ebcdd9e9a1968d0b5d92ff14795af
anka07 <anka07@reven.mac.local>
2019-04-21 00:05:51 +0200

    Should have never exposed the api key in the git history. Should fetch the value from environmental variables instead. Use .env (gitignored) for local development.

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,12 +2,16 @@
 
 const config = require('../config/index')
 const fetch = require('../fetch')
+const dotenv = require('dotenv')
+
+dotenv.config();
+const key = process.env.API_KEY;
 
 const getImdb = (title) => {
   // http://localhost:5000/trailer?title=para-knas-2017
   // https://content.viaplay.se/pc-se/film/para-knas-2017
-  // https://api.themoviedb.org/3/find/tt6205766?api_key=ddc5cfb794251973f585a5fd7768930e&language=en-US&external_source=imdb_id
-  // https://api.themoviedb.org/3/movie/492867/videos?api_key=ddc5cfb794251973f585a5fd7768930e
+  // https://api.themoviedb.org/3/find/tt6205766?api_key=API_KEY&language=en-US&external_source=imdb_id
+  // https://api.themoviedb.org/3/movie/492867/videos?api_key=API_KEY
   const contentViaplayURL = `${config.contentViaplayURL}/${title}`
 
   return fetch(contentViaplayURL)
@@ -16,12 +20,14 @@ const getImdb = (title) => {
 const getMovie = (imdbId) => {
   // http://localhost:5000/trailer?title=para-knas-2017
   // https://content.viaplay.se/pc-se/film/para-knas-2017
-  // https://api.themoviedb.org/3/find/tt6205766?api_key=ddc5cfb794251973f585a5fd7768930e&language=en-US&external_source=imdb_id
-  // https://api.themoviedb.org/3/movie/492867/videos?api_key=ddc5cfb794251973f585a5fd7768930e
+  // https://api.themoviedb.org/3/find/tt6205766?api_key=API_KEY&language=en-US&external_source=imdb_id
+  // https://api.themoviedb.org/3/movie/492867/videos?api_key=API_KEY
   const apiTheMovieDbURL = `${config.apiTheMovieDb}/find/${imdbId}`
   // console.log('imdb_id getMovie',imdb_id)
+  
+
   const movieParams = {
-    api_key: 'ddc5cfb794251973f585a5fd7768930e',
+    api_key: key,
     language: 'en-US',
     external_source: 'imdb_id'
   }
@@ -31,12 +37,12 @@ const getMovie = (imdbId) => {
 const getTrailer = (movieId) => {
   // http://localhost:5000/trailer?title=para-knas-2017
   // https://content.viaplay.se/pc-se/film/para-knas-2017
-  // https://api.themoviedb.org/3/find/tt6205766?api_key=ddc5cfb794251973f585a5fd7768930e&language=en-US&external_source=imdb_id
-  // https://api.themoviedb.org/3/movie/492867/videos?api_key=ddc5cfb794251973f585a5fd7768930e
+  // https://api.themoviedb.org/3/find/tt6205766?api_key=API_KEY&language=en-US&external_source=imdb_id
+  // https://api.themoviedb.org/3/movie/492867/videos?api_key=API_KEY
   const url = `${config.apiTheMovieDb}/movie/${movieId}/videos`
   // console.log('movie_id getTrailer', movie_id)
   const trailerParams = {
-    api_key: 'ddc5cfb794251973f585a5fd7768930e'
+    api_key: key
   }
   return fetch(url, trailerParams)
 }
